d6c195ba906ca37b6d647fdbfe4b3a33ab3e33bd
opilane <opilane@FUJI-YMHP016308>
2023-06-05 16:21:39 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Dictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var schools = new Dictionary<string, string>();
+
+            schools.Add("1", "Tallinna Kunstigümnaasium");
+            schools.Add("2", "Tallinna Tööstushariduskeskus");
+            schools.Add("3", "Tallinna Mustamäe Gümnaasium");
+
+            foreach (var school in schools)
+            {
+                Console.WriteLine($"{school.Key} - {school.Value}");
+            }
+            Console.WriteLine("----------------------------");
+            Console.WriteLine(schools["1"]);
+            Console.WriteLine(schools["2"]);
+            if (schools.ContainsKey("Tallinna Tööstushariduskeskus"))
+            {
+                Console.WriteLine(schools["2"]);
+            }
+            else
+            {
+                Console.WriteLine("Rohkem ei ole koole nimega Tallinna Tööstushariduskeskus");
+            }
+        }
+    }
+}
